befacb3027ebf8d89ed76a53c60c2e60839b95c0
Svit Spindler <svit.spindler@gmail.com>
2024-03-21 21:47:11 +0100

    database set up

--- src/main/java/com/example/repository/LogEntityRepository.java
+++ src/main/java/com/example/repository/LogEntityRepository.java
@@ -2,6 +2,8 @@ package com.example.repository;
 
 import com.example.entity.LogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public abstract class LogEntityRepository implements JpaRepository<LogEntity, Long>{
+@Repository
+public interface LogEntityRepository extends JpaRepository<LogEntity, Long>{
 }
--- src/main/java/com/example/service/PythonCodeService.java
+++ src/main/java/com/example/service/PythonCodeService.java
@@ -6,6 +6,8 @@ import com.example.repository.LogEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class PythonCodeService {
 
